0005ebdc58268c79479c35d91ed7170366f09488
inzapp <inzapp@naver.com>
2021-07-19 10:33:32 +0900

    remove default cycle lr setting

--- train.py
+++ train.py
@@ -5,8 +5,6 @@ if __name__ == '__main__':
         train_image_path=r'./train',
         validation_image_path=r'./validation',
         input_shape=(128, 128, 1),
-        max_lr=0.01,
-        min_lr=1e-4,
-        cycle_steps=2000,
+        lr=1e-3,
         batch_size=32,
-        epochs=1000).fit()
+        epochs=300).fit()
