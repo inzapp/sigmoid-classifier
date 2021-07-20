162cf175b0da6a16e4df18ea6d7cef6961e938f6
inzapp <inzapp@naver.com>
2021-07-20 09:53:56 +0900

    change parameter name : cycle_steps -> cycle_length, add no evaluation save model part

--- cosine_lr_decay.py
+++ cosine_lr_decay.py
@@ -11,7 +11,7 @@ class CosineLRDecay(tf.keras.callbacks.Callback):
             max_lr=0.1,
             min_lr=0.001,
             batch_size=32,
-            cycle_steps=2000,
+            cycle_length=2000,
             train_data_generator_flow=None,
             validation_data_generator_flow=None):
         self.max_val_recall = 0.0
@@ -19,7 +19,7 @@ class CosineLRDecay(tf.keras.callbacks.Callback):
         self.max_lr = max_lr
         self.min_lr = min_lr
         self.batch_size = batch_size
-        self.cycle_steps = cycle_steps
+        self.cycle_length = cycle_length
         self.train_data_generator_flow = train_data_generator_flow
         self.validation_data_generator_flow = validation_data_generator_flow
         super().__init__()
@@ -33,16 +33,19 @@ class CosineLRDecay(tf.keras.callbacks.Callback):
 
     def update(self, model):
         self.model = model
-        lr = self.min_lr + 0.5 * (self.max_lr - self.min_lr) * (1.0 + np.cos(((1.0 / (0.5 * self.cycle_steps)) * np.pi * self.batch_sum) + np.pi))
+        lr = self.min_lr + 0.5 * (self.max_lr - self.min_lr) * (1.0 + np.cos(((1.0 / (0.5 * self.cycle_length)) * np.pi * self.batch_sum) + np.pi))
         tf.keras.backend.set_value(self.model.optimizer.lr, lr)
         self.batch_sum += 1
-        if self.batch_sum % self.cycle_steps == 0:
+        if self.batch_sum % self.cycle_length == 0:
             self.save_model()
 
     def save_model(self):
-        recall = self.model.evaluate(x=self.train_data_generator_flow, batch_size=self.batch_size, return_dict=True)['recall']
-        val_recall = self.model.evaluate(x=self.validation_data_generator_flow, batch_size=self.batch_size, return_dict=True)['recall']
-        if val_recall > self.max_val_recall:
-            self.max_val_recall = val_recall
-            print(f'{self.batch_sum} batch => recall: {recall:.4f}, val_recall: {val_recall:.4f}\n')
-            self.model.save(f'checkpoints/model_{self.batch_sum}_batch_recall_{recall:.4f}_val_recall_{val_recall:.4f}.h5')
+        if self.train_data_generator_flow is None or self.validation_data_generator_flow is None:
+            self.model.save(f'checkpoints/model_{self.batch_sum}_batch.h5')
+        else:
+            recall = self.model.evaluate(x=self.train_data_generator_flow, batch_size=self.batch_size, return_dict=True)['recall']
+            val_recall = self.model.evaluate(x=self.validation_data_generator_flow, batch_size=self.batch_size, return_dict=True)['recall']
+            if val_recall > self.max_val_recall:
+                self.max_val_recall = val_recall
+                print(f'{self.batch_sum} batch => recall: {recall:.4f}, val_recall: {val_recall:.4f}\n')
+                self.model.save(f'checkpoints/model_{self.batch_sum}_batch_recall_{recall:.4f}_val_recall_{val_recall:.4f}.h5')
