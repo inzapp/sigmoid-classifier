c8a568fb6099bddf1e63847e49d96352e2ae8af6
inzapp <inzapp@naver.com>
2024-02-12 19:11:28 +0900

    refactor layer function naming

--- model.py
+++ model.py
@@ -30,54 +30,51 @@ class Model:
 
     def build(self):
         input_layer = tf.keras.layers.Input(shape=self.input_shape, name='sc_input')
-        x = self.__conv_block(input_layer, 16, 3)
+        x = self.conv2d(input_layer, 16, 3)
         if self.is_stride_over(2):
-            x = self.__max_pool(x)
+            x = self.maxpooling2d(x)
 
-        x = self.__dropout(x, 0.1)
-        x = self.__conv_block(x, 32, 3)
+        x = self.dropout(x, 0.1)
+        x = self.conv2d(x, 32, 3)
         if self.is_stride_over(4):
-            x = self.__max_pool(x)
+            x = self.maxpooling2d(x)
 
-        x = self.__dropout(x, 0.15)
-        x = self.__conv_block(x, 64, 3)
+        x = self.dropout(x, 0.15)
+        x = self.conv2d(x, 64, 3)
         if self.is_stride_over(8):
-            x = self.__max_pool(x)
+            x = self.maxpooling2d(x)
 
-        x = self.__dropout(x, 0.2)
-        x = self.__conv_block(x, 128, 3)
+        x = self.dropout(x, 0.2)
+        x = self.conv2d(x, 128, 3)
         if self.is_stride_over(16):
-            x = self.__max_pool(x)
+            x = self.maxpooling2d(x)
 
-        x = self.__dropout(x, 0.25)
-        x = self.__conv_block(x, 256, 3, cam_activation=True)
+        x = self.dropout(x, 0.25)
+        x = self.conv2d(x, 256, 3, cam_activation=True)
         if self.is_stride_over(32):
-            x = self.__max_pool(x)
+            x = self.maxpooling2d(x)
 
-        x = self.__dropout(x, 0.3)
-        x = self.__conv_block(x, 256, 3)
-        output_layer = self.__classification_layer(x)
+        x = self.dropout(x, 0.3)
+        x = self.conv2d(x, 256, 3)
+        output_layer = self.classification_layer(x)
         return tf.keras.models.Model(input_layer, output_layer)
 
     def is_stride_over(self, stride):
         return self.input_shape[0] >= stride and self.input_shape[1] >= stride
 
-    def __conv_block(self, x, filters, kernel_size, bn=False, cam_activation=False):
-        x = self.__conv(x, filters, kernel_size, use_bias=False if bn else True)
+    def conv2d(self, x, filters, kernel_size, bn=False, cam_activation=False):
+        x = tf.keras.layers.Conv2D(
+            filters=filters,
+            padding='same',
+            use_bias=not bn,
+            kernel_size=kernel_size,
+            kernel_initializer='he_normal')(x)
         if bn:
             x = tf.keras.layers.BatchNormalization()(x)
         x = tf.keras.layers.Activation('relu', name=self.cam_activation_layer_name if cam_activation else None)(x)
         return x
 
-    def __conv(self, x, filters, kernel_size, use_bias=True):
-        return tf.keras.layers.Conv2D(
-            filters=filters,
-            kernel_size=kernel_size,
-            kernel_initializer='he_normal',
-            padding='same',
-            use_bias=use_bias)(x)
-
-    def __classification_layer(self, x, name='sc_output'):
+    def classification_layer(self, x, name='sc_output'):
         x = tf.keras.layers.Conv2D(
             filters=self.num_classes,
             kernel_size=1,
@@ -86,10 +83,9 @@ class Model:
             name=self.last_conv_layer_name)(x)
         return tf.keras.layers.GlobalAveragePooling2D(name=name)(x)
 
-    def __max_pool(self, x):
-        return tf.keras.layers.MaxPool2D()(x)
+    def maxpooling2d(self, x):
+        return tf.keras.layers.MaxPooling2D()(x)
 
-    @staticmethod
-    def __dropout(x, rate):
+    def dropout(self, x, rate):
         return tf.keras.layers.Dropout(rate)(x)
 
